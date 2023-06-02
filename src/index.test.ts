a07f0d08e043a161b58c170deb20f24af4d08513
Tristan F <LeoDog896@hotmail.com>
2023-06-01 20:37:57 -0400

    feat: base grid rotation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
-import { createBoard, getClearedLines, clearLines } from './lib/board';
+import { createBoard, getClearedLines, clearLines } from '$lib/board';
+import { asBooleanArray, rotate } from '$lib/piece';
 
 describe('board', () => {
 	describe('createBoard', () => {
@@ -7,6 +8,42 @@ describe('board', () => {
 			const board = createBoard(10, 20);
 			expect(board.length).toBe(20);
 			expect(board[0].length).toBe(10);
+		});
+	});
+});
+
+describe('piece', () => {
+	describe('asBooleanArray', () => {
+		it('should convert a 2D array of 0s and 1s to a 2D array of booleans', () => {
+			const array = asBooleanArray([
+				[0, 0, 0, 0],
+				[1, 1, 1, 1],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			]);
+			expect(array).toEqual([
+				[false, false, false, false],
+				[true, true, true, true],
+				[false, false, false, false],
+				[false, false, false, false]
+			]);
+		});
+	});
+
+	describe('rotate', () => {
+		it('should rotate a 2D array', () => {
+			const array = rotate(asBooleanArray([
+				[0, 0, 0, 0],
+				[1, 1, 1, 1],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			]));
+			expect(array).toEqual([
+				[false, true, false, false],
+				[false, true, false, false],
+				[false, true, false, false],
+				[false, true, false, false]
+			]);
 		});
 	});
 });
--- src/lib/piece.ts
+++ src/lib/piece.ts
@@ -1,4 +1,42 @@
+type Rotation = boolean[][];
+
 interface Piece {
+    id: string;
     color: string;
-    grid: number[][];
+    rotations: Rotation[];
 }
+
+export function asBooleanArray(array: (0 | 1)[][]): boolean[][] {
+    return array.map((value) => value.map((v) => v === 1));
+}
+
+// TODO this is broken!
+export function rotate(arr: boolean[][]): boolean[][] {
+    const n = arr.length;
+    const x = Math.floor(n / 2);
+    const y = n - 1;
+    for (let i = 0; i < x; i++) {
+        for (let j = i; j < y - i; j++) {
+            const k = arr[i][j];
+            arr[i][j] = arr[y - j][i];
+            arr[y - j][i] = arr[y - i][y - j];
+            arr[y - i][y - j] = arr[j][y - i];
+            arr[j][y - i] = k;
+        }
+    }
+    return arr;
+}
+
+// // data referenced from https://tetris.wiki/Super_Rotation_System
+// const pieces: Piece[] = [
+//     {
+//         id: 'I',
+//         color: 'cyan',
+//         rotations: [
+//             [
+//                 asBooleanArray([0, 0, 0, 0]),
+//                 asBooleanArray([1, 1, 1, 1]),
+//                 asBooleanArray([0, 0, 0, 0]),
+//                 asBooleanArray([0, 0, 0, 0]),
+//             ],
+//             [
